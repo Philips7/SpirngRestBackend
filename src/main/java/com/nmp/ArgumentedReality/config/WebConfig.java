5b1cb6839634773f8918238014974c20870528a8
Dominik <dominik.filip10@gmail.com>
2017-05-26 01:50:44 +0200

    cors10

--- src/main/java/com/nmp/ArgumentedReality/config/WebConfig.java
+++ src/main/java/com/nmp/ArgumentedReality/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("localhost:4200")
                 .allowedMethods("*")
                 .allowedHeaders("*");
 //                .exposedHeaders("authorization", "content-type", "accept")
